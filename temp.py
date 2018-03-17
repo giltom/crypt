840c2376c1ab9a6d4d728ca5eb1b89c1110abde3
Gilad Ben Dov <giladbendov@gmail.com>
2018-03-17 21:01:49 +0200

    do stuff

--- temp.py
+++ temp.py
@@ -1,18 +1,22 @@
 from crypt import *
-
 from pwn import *
-
 import socket
 
 f = open('data/pk')
 n = int(f.readline())
 x = int(f.readline())
-aes_key = b'\0'*16
+print(n)
+print(x)
+print(n > x)
+aes_key = b'\xFF'*16
 enc_aes_key = gm_encrypt(aes_key, x, n)
+print(len(enc_aes_key))
 
-sock = socket.create_connection(('web.angstromctf.com', 3000))
+sock = remote('web.angstromctf.com', 3000)
 
-
+print(sock.recvuntil('format.\n'))
 for val in enc_aes_key:
-	sock.sendall(str(val).encode('UTF-8'))
-print(sock.recv(4098))
+	sock.sendline(str(val))
+cipherb64 = sock.recvall()[:-1]
+cipher = base642bytes(cipherb64)
+print(aes_decrypt(cipher[16:], aes_key, modes.CFB8(cipher[:16])))
