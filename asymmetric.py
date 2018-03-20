6f5cfc7750d4785895c18cd00a85328922080705
Gilad Ben Dov <giladbendov@gmail.com>
2018-03-20 17:42:42 +0200

    Add RSA key stuff

--- asymmetric.py
+++ asymmetric.py
@@ -1,6 +1,7 @@
 import random
 
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
 
 from crypt import numbers as num
 from crypt import encodings as enc
@@ -51,3 +52,24 @@ def parse_ssh_rsa_public_key(fname):
     f.close()
     nums = key.public_numbers()
     return nums.n, nums.e
+
+#Return the prime factors p,q of n. e is the public exponent and d is the private exponent.
+def rsa_factor_given_private_key(n, e, d):
+    return rsa.rsa_recover_prime_factors(n, e, d)
+
+#Creates a PEM-format RSA private key in the file fname, using the modulus n, public exponent e and private exponent d
+def make_pem_rsa_key(fname, n, e, d):
+    p, q = rsa_factor_given_private_key(n, e, d)
+    dmp1 = d % (p - 1)
+    dmq1 = d % (q - 1)
+    iqmp = num.mod_inverse(q, p)
+    pub_nums = rsa.RSAPublicNumbers(e, n)
+    priv_nums = rsa.RSAPrivateNumbers(p, q, d, dmp1, dmq1, iqmp, pub_nums)
+    key = priv_nums.private_key(const.BACKEND)
+    pem = key.private_bytes(
+        serialization.Encoding.PEM,
+        serialization.PrivateFormat.TraditionalOpenSSL,
+        serialization.NoEncryption()
+    )
+    with open(fname, 'wb') as f:
+        f.write(pem)
